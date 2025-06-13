ab0a9e06c593448c5ddaeacc5c5b4417ae24106b
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-13 14:53:46 +0200

    Generate SEO-friendly profile links

--- prov-be.php
+++ prov-be.php
@@ -37,7 +37,7 @@
 		<div class="row" v-cloak>
       <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item" id="Slankie" v-for="profile in filtered_profiles">
         <div class="card h-100">
-          <a :href="'profile.php?country=be&id=' + profile.id"><img class="card-img-top" :src="profile.src.replace('150x150', '300x300')" :alt="profile.name + ' daten in Flevoland'" @error="imgError"></a>
+          <a :href="'shemale-' + profile.slug + '?id=' + profile.id"><img class="card-img-top" :src="profile.src.replace('150x150', '300x300')" :alt="profile.name + ' daten in Flevoland'" @error="imgError"></a>
           <div class="card-body">
             <div class="card-top">
                 <h4 class="card-title">{{ profile.name }}</h4>  
@@ -49,7 +49,7 @@
               <li class="list-group-item">Provincie: {{ profile.province }}</li>
             </ul>
           </div>
-          <a :href="'profile.php?country=be&id=' + profile.id" class="card-footer btn btn-primary">Bekijk profiel</a>
+          <a :href="'shemale-' + profile.slug + '?id=' + profile.id" class="card-footer btn btn-primary">Bekijk profiel</a>
         </div>
       </div>
       <script>
